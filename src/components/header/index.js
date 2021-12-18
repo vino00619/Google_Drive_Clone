5d095dcb0e7723662a552799a28b53b889f1d176
vino00619 <vinoth00619@gmail.com>
2021-12-18 18:03:55 +0530

    header start

---
+++ src/components/header/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import GDriveLogo from '../../media/Google_Drive_icon_(2020).svg.png'
+import SearchIcon from '@mui/icons-material/Search';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+const index = () => {
+    return (
+        <div className='header'>
+            <div className='header_logo'>
+                <img src={GDriveLogo} alt=''/>
+                <span>Drive</span>
+            </div>
+            <div className='header_searchContainer'>
+                <SearchIcon />
+            </div>
+            <div className='header_icons'></div>
+        </div>
+    )
+}
+
+export default index
